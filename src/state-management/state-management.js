a85c332918710db58a76722f5994eab8037d3860
Norman Coloma García <ua.norman@gmail.com>
2018-03-24 15:13:32 +0100

    Adding tests for handling state functions

--- src/state-management/state-management.js
+++ src/state-management/state-management.js
@@ -1,47 +1,33 @@
 const NO_SELECTED_ITEM_INDEX = -1;
 const EMPTY_SUGGESTIONS = 0;
 
-const addNewSearch = (term) => {
-    return function update(state) {
-        const { recentSearches } = this.state;
-        const termNotFound = !recentSearches.includes(term);
+const addNewSearch = (term) => (state) => {
+    const { recentSearches } = state;
+    const termNotFound = !recentSearches.includes(term);
 
-        let newSearches = recentSearches;
+    const newSearches = termNotFound ? [...recentSearches, term] : recentSearches;
 
-        if (termNotFound) {
-            newSearches = [...this.state.recentSearches, term];
-        }
-
-        return {
-            showSuggestions: false, suggestions: [],
-            recentSearches: newSearches, term, selectedItemIndex: NO_SELECTED_ITEM_INDEX
-        };
+    return {
+        showSuggestions: false, suggestions: [],
+        recentSearches: newSearches, term, selectedItemIndex: NO_SELECTED_ITEM_INDEX
     }
 }
 
-const resetAndHideSuggestions = (state) => { 
-    return { showSuggestions: false, suggestions: [], selectedItemIndex: NO_SELECTED_ITEM_INDEX }; 
+const resetAndHideSuggestions = (state) => {
+    return { showSuggestions: false, suggestions: [], selectedItemIndex: NO_SELECTED_ITEM_INDEX };
 }
 
-const hideSuggestions = (state) =>{
+const hideSuggestions = (state) => {
     return { showSuggestions: false, selectedItemIndex: NO_SELECTED_ITEM_INDEX };
-} 
-
-const selectItemIndex = (selectedItemIndex) => {
-    return function update(state) {
-        return {
-            selectedItemIndex
-        }
-    }
 }
 
-const establishSuggestionsForTerm = (suggestions, term) => {
-    return function update(state) {
-        if (suggestions.length > EMPTY_SUGGESTIONS) {
-            return { showSuggestions: true, suggestions, term };
-        }
-        return { showSuggestions: false, suggestions, term, selectedItemIndex: NO_SELECTED_ITEM_INDEX };
+const selectItemIndex = (selectedItemIndex) => (state) => ({ selectedItemIndex});
+
+const establishSuggestionsForTerm = (suggestions, term) => (state) => {
+    if (suggestions.length > EMPTY_SUGGESTIONS) {
+        return { showSuggestions: true, suggestions, term };
     }
+    return { showSuggestions: false, term, selectedItemIndex: NO_SELECTED_ITEM_INDEX };
 }
 
 export { addNewSearch, resetAndHideSuggestions, hideSuggestions, selectItemIndex, establishSuggestionsForTerm };
---
+++ src/state-management/state-management.test.js
@@ -0,0 +1,88 @@
+import { addNewSearch, resetAndHideSuggestions, hideSuggestions, selectItemIndex, establishSuggestionsForTerm } from './state-management';
+
+test('should add new term to recentSearches when term not found', () => {
+    const term = 'star wars';
+    const setStateFn = addNewSearch(term);
+
+    const initialState = {
+        showSuggestions: false,
+        suggestions: [],
+        recentSearches: [],
+        selectedItemIndex: -1,
+        term: ""
+    };
+
+    const expectedState = Object.assign({}, initialState, { term, recentSearches: [term] });
+
+    expect(setStateFn(initialState)).toEqual(expectedState);
+});
+
+test('should not add new term to recentSearches when term found', () => {
+    const term = 'star wars';
+    const setStateFn = addNewSearch(term);
+
+    const initialState = {
+        showSuggestions: false,
+        suggestions: [],
+        recentSearches: [term],
+        selectedItemIndex: -1,
+        term: ""
+    };
+    const expectedState = Object.assign({}, initialState, { term });
+
+    expect(setStateFn(initialState)).toEqual(expectedState);
+});
+
+test('should reset and hide suggestions from state', () => {
+    const term = 'star wars';
+    const initialState = {
+        showSuggestions: true,
+        suggestions: [term],
+        selectedItemIndex: -1
+    };
+
+    const expectedState = { suggestions: [], selectedItemIndex: -1, showSuggestions: false };
+
+    expect(resetAndHideSuggestions(initialState)).toEqual(expectedState);
+});
+
+test('should hide suggestions', () => {
+    const initialState = { showSuggestions: true, selectedItemIndex: 0 };
+    const expectedState = { showSuggestions: false, selectedItemIndex: -1 };
+
+    expect(hideSuggestions(initialState)).toEqual(expectedState);
+});
+
+test('should change selectedItemIndex', () => {
+    const initialState = { selectedItemIndex: 0 };
+    const expectedState = { selectedItemIndex: 1 };
+    const setStateFn = selectItemIndex(1);
+
+    expect(setStateFn(initialState)).toEqual(expectedState);
+});
+
+test('should establish suggestions that match given term and show them', () => {
+    const suggestions = ['star wars'];
+    const term = 'star wars';
+
+    const initialState = { showSuggestions: false, suggestions: [], term: "" };
+    
+    const setStateFn = establishSuggestionsForTerm(suggestions, term);
+    const expectedState = { showSuggestions: true, suggestions, term };
+
+    expect(setStateFn(initialState)).toEqual(expectedState);
+    
+});
+
+test('should establish suggestions and term, but not showing them because does not match term passed', () => {
+    const suggestions = [];
+    const term = 'shutter island';
+
+    const initialState = { showSuggestions: false, suggestions: [], term: "" };
+    
+    const setStateFn = establishSuggestionsForTerm(suggestions, term);
+    const expectedState = { showSuggestions: false, term, selectedItemIndex: -1 };
+
+    expect(setStateFn(initialState)).toEqual(expectedState);
+    
+});

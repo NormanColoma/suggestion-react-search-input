53c15a5c2c94eeb28d825cb30393e6045ac81d69
Norman Coloma García <ua.norman@gmail.com>
2019-06-09 23:47:20 +0200

    Fixing test

--- src/state-management/local-storage/local-storage.test.js
+++ src/state-management/local-storage/local-storage.test.js
@@ -1,70 +1,56 @@
 import { saveSearches, loadSearches } from './local.storage';
 
+beforeEach(() => {
+    jest.spyOn(Storage.prototype, 'setItem');
+    jest.spyOn(Storage.prototype, 'getItem');
+});
+
+afterEach(() =>  {
+    localStorage.setItem.mockRestore();
+    localStorage.getItem.mockRestore();
+});
+  
 test('should save seraches to local storage', () => {
     const setItemMock = jest.fn();
     const searches = ['star wars'];
-
-    const localStorageMock = {
-        setItem: setItemMock
-    }
-    global.localStorage = localStorageMock;
     
     saveSearches(searches);
 
-    expect(setItemMock.mock.calls.length).toBe(1);
+    expect(localStorage.setItem).toHaveBeenCalledTimes(1);
 });
 
 test('should call console error while saving data to local storage and error produced', () => {
     const setItemMock = jest.fn(() => { throw new Error() });
     const consoleErrorMock = jest.fn();
     const searches = ['star wars'];
-    const localStorageMock = { setItem: setItemMock };
-    const consoleMock = { error: consoleErrorMock };
 
-    global.localStorage = localStorageMock;
-    global.console = consoleMock;
+    jest.spyOn(console, 'error');
+    jest.spyOn(Storage.prototype, 'setItem').mockImplementation(() => { throw new Error() });
 
     saveSearches(searches);
-    expect(consoleErrorMock.mock.calls.length).toBe(1);
+    expect(console.error).toHaveBeenCalledTimes(1);
 });
 
 test('should load seraches from local storage', () => {
     const searches = ['star wars'];
-    const getItemMock = jest.fn();
-    const localStorageMock = {
-        getItem: getItemMock
-    }
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation(() => JSON.stringify(searches)); 
 
-    getItemMock.mockReturnValue(JSON.stringify(searches));
-    global.localStorage = localStorageMock;
-    
     expect(loadSearches(searches)).toEqual(searches);
-    expect(getItemMock.mock.calls.length).toBe(1);
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
 });
 
 test('should return empty array when seraches key is not yet set in local storage', () => {
     const searches = ['star wars'];
-    const getItemMock = jest.fn();
-    const localStorageMock = {
-        getItem: getItemMock
-    }
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation(() => null);
 
-    getItemMock.mockReturnValue(null);
-    global.localStorage = localStorageMock;
-    
     expect(loadSearches(searches)).toEqual([]);
-    expect(getItemMock.mock.calls.length).toBe(1);
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
 });
 
 test('should call console error while retrieving data from local storage and error produced', () => {
-    const getItemMock = jest.fn(() => { throw new Error() });
-    const consoleErrorMock = jest.fn();
-    const localStorageMock = { getItem: getItemMock };
-    const consoleMock = { error: consoleErrorMock };
-
-    global.localStorage = localStorageMock;
-    global.console = consoleMock;
+    jest.spyOn(Storage.prototype, 'getItem').mockImplementation(() => { throw new Error() });
 
     loadSearches();
-    expect(consoleErrorMock.mock.calls.length).toBe(1);
+    
+    expect(localStorage.getItem).toHaveBeenCalledTimes(1);
 });
